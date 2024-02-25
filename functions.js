50b18a9e7cda16dab713d2981565391ddec3d7a9
itsmesalim <itsme.salimullah@gmail.com>
2024-02-25 09:49:28 +0500

    Commit for test

--- functions.js
+++ functions.js
@@ -1,4 +1,8 @@
-//function: its devide a large program into small pieces of code. 
+/*function: A function is a block of reusable code that performs a specific task. 
+It can take inputs, perform operations, and return outputs. 
+Functions help organize code, promote reusability, and 
+make programs easier to understand and maintain. */
+
 function sumFunction(a , b){
     var sumOfTwoNumbers = a + b;
 
@@ -70,8 +74,12 @@ function carBill(){
 
 carBill(889, 983, 234, 904, 342, 592);
 
-//recusion:
-console.log("when a function calls itself are called recusion");
+console.log("Recusion: when a function calls itself are called recusion");
+/* 
+    It's particularly useful for solving problems that 
+    can be broken down into smaller, similar subproblems. 
+    For example, computing factorials:
+*/
 function factorial(number){
     
     if(number == 1){
@@ -95,14 +103,14 @@ function defaultFunction(a = 6){
 defaultFunction();  // thier the parameter value will be print automaticaly
 defaultFunction(8); //thier the parameter value will be ignored and assigned the value of function call to the parameter
 
-function defaultFunction1(name, age, address, marks){
+function defaultFunction1(name = "Salim Ullah", age = 23, address = "Islamabad", marks =  79){
     console.log("my name is "+name);
     console.log("I have "+age+" years old");
     console.log("I am from "+address);
     console.log("I have got "+marks+" marks in my subject");
 }
 
-defaultFunction1("Salim Ullah", 23, "Islamabad", 79);
+defaultFunction1();
 
 /*Scope of variable: their are three type of scope of variable like:
 1) global variable: which is accessable everywhere in the entire program.
@@ -168,5 +176,130 @@ function logIn(username, password){
 console.log(logIn("itsme.salim", 123));
 console.log(logIn("this.me", 34587));
 
-console.log("call back function ");
+console.log("call back function: a function takes another function as an arguments is callback function");
 
+//define a callback function
+function callBack1(name, age){
+
+    console.log(`I am ${name}, and I have ${age}`);
+}
+
+function doWork(name, age, callback){
+    name = "Mr "+name;
+    age++;
+    callback(name, age);
+}
+doWork("Salim Ullah", 40, callBack1);
+
+function year (year, month){
+    console.log(`year : ${year}, month : ${month}`)
+}
+
+function curentYear(year, month, callback){
+    callback(year, month);
+}
+curentYear(2024, "february", year);
+
+
+console.log("set timeout function: it's call the callback function at once in a spacipic time of seconds.");
+function sayHello(){
+    console.log("hello");
+}
+
+function sayGoodBy(){
+    console.log("good By");
+    sayHello();
+}
+
+/*
+    setTimeout(sayGoodBy, 5*1000); //setTimeout function invoke the call back (sayGoodBy) function after 5 seconds.
+
+    console.log("setInterval function: it's like a setTimeout but it's exicute the callback function multiple time repeatedly.");
+    // Callback function
+    function callBack(){
+        console.log("Hello");
+    }
+
+    // Set an interval to call callBack function every 2000 milliseconds (2 seconds).
+    let myInterval = setInterval(callBack, 2000);
+
+    // Define a function to stop the interval after 20 seconds.
+    function stopInterval(){
+        clearInterval(myInterval);
+    }
+    // Set a timeout to call stopInterval function after 20 seconds.
+    setTimeout(stopInterval, 20*1000);
+*/
+
+/*Higher Order Function: it take one or more function as arguments or
+return a function as its arguments.*/
+function add2Numbers(a, b, sum){
+    return sum(a , b);
+}
+function add(a , b){
+    return a + b;
+}
+console.log(add2Numbers(14 , 50, add));
+
+/*Curring: */
+
+function curry(arg1){
+    return function(arg2){
+        return function(arg3){
+            return function(){
+                let name = arg1;
+                let age = arg2;
+                let address = arg3;
+
+                return {name, age, address};
+            }
+        }
+    }
+}
+console.log(curry("Salim Ullah")(24)("Islamabad")());
+
+//another example
+function nestedFunctions(vaue1){
+    return function(value2){
+        let mul1 = vaue1 * value2;
+        
+        return function(value3){
+            let mul2 = mul1 * value3;
+            return mul2;
+        }
+    }
+}
+console.log(nestedFunctions(10)(20)(30));
+
+//another example
+function sendAndRecieveMassage(reciever){
+    let result1 = `@${reciever}`;
+
+    return function(massage){
+        let result2 = `${result1} : ${massage}`;
+        
+        return function(hour, minuts, timePeriod){
+            let result3 = `${result2} at ${hour} : ${minuts} ${timePeriod}`;
+
+            return function (sender){
+                let result4 = `${result3} - from ${sender}`;
+
+                return result4;
+            }
+        }
+    }
+}
+console.log(sendAndRecieveMassage("Salim Ullah")("Hi how are you?")(10, 30, "am")("Ubaid"));
+
+//test
+function addPy(input) {
+    if (input.startsWith("Py")) {
+        return input; 
+    } else {
+        return "Py" + input; 
+    }
+}
+
+// Example usage:
+console.log(addPy("thon")); 
+console.log(addPy("Pythagoras"));
